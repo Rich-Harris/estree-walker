3dabadfeb29f32a4b4c8611919d7fc726d54388f
Rich-Harris <richard.a.harris@gmail.com>
2015-10-22 23:22:50 -0400

    add es6 build

--- gobblefile.js
+++ gobblefile.js
@@ -1,12 +1,20 @@
 var gobble = require( 'gobble' );
 
-module.exports = gobble( 'src' )
-	.transform( 'babel', {
-		blacklist: [ 'es6.modules', 'useStrict' ]
+module.exports = gobble([
+	// UMD build
+	gobble( 'src' ).transform( 'rollup-babel', {
+		entry: 'estree-walker.js',
+		dest: 'estree-walker.umd.js',
+		format: 'umd',
+		moduleName: 'estreeWalker',
+		sourceMap: true
+	}),
+
+	// ES6 build
+	gobble( 'src' ).transform( 'rollup-babel', {
+		entry: 'estree-walker.js',
+		dest: 'estree-walker.es6.js',
+		format: 'es6',
+		sourceMap: true
 	})
-	.transform( 'esperanto', {
-		type: 'umd',
-		name: 'estreeWalker',
-		strict: true
-	})
-	.transform( 'sorcery' );
+]);
