f55e349aadbcdbc60afc46501a0f9573adf75154
Rich-Harris <richard.a.harris@gmail.com>
2016-09-10 09:11:17 -0400

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
 import buble from 'rollup-plugin-buble';
 
+const pkg = require( './package.json' );
+
 export default {
 	entry: 'src/estree-walker.js',
+	targets: [
+		{ dest: pkg.main, format: 'umd' },
+		{ dest: pkg.module, format: 'es' }
+	],
 	moduleName: 'estreeWalker',
 	plugins: [ buble() ],
 	sourceMap: true
