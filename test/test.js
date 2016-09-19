bbeb89f8c0645ba6cc49066d7cce6f2da7b845ed
Rich Harris <richard.a.harris@gmail.com>
2016-09-19 12:54:15 -0400

    update tests

--- test/test.js
+++ test/test.js
@@ -8,11 +8,18 @@ describe( 'estree-walker', () => {
 			type: 'Program',
 			body: [{
 				type: 'VariableDeclaration',
-				declarations: [{
-					type: 'VariableDeclarator',
-					id: { type: 'Identifier', name: 'a' },
-					init: { type: 'Literal', value: 1, raw: '1' }
-				}],
+				declarations: [
+					{
+						type: 'VariableDeclarator',
+						id: { type: 'Identifier', name: 'a' },
+						init: { type: 'Literal', value: 1, raw: '1' }
+					},
+					{
+						type: 'VariableDeclarator',
+						id: { type: 'Identifier', name: 'b' },
+						init: { type: 'Literal', value: 2, raw: '2' }
+					}
+				],
 				kind: 'var'
 			}],
 			sourceType: 'module'
@@ -30,18 +37,68 @@ describe( 'estree-walker', () => {
 			}
 		});
 
-		assert.equal( entered.length, 5 );
-		assert.equal( left.length, 5 );
-
-		[
+		assert.deepEqual( entered, [
 			ast,
 			ast.body[0],
 			ast.body[0].declarations[0],
 			ast.body[0].declarations[0].id,
-			ast.body[0].declarations[0].init
-		].forEach( node => {
-			assert.ok( ~entered.indexOf( node ) );
-			assert.ok( ~left.indexOf( node ) );
+			ast.body[0].declarations[0].init,
+			ast.body[0].declarations[1],
+			ast.body[0].declarations[1].id,
+			ast.body[0].declarations[1].init
+		]);
+
+		assert.deepEqual( left, [
+			ast.body[0].declarations[0].id,
+			ast.body[0].declarations[0].init,
+			ast.body[0].declarations[0],
+			ast.body[0].declarations[1].id,
+			ast.body[0].declarations[1].init,
+			ast.body[0].declarations[1],
+			ast.body[0],
+			ast
+		]);
+	});
+
+	it( 'handles null literals', () => {
+		const ast = {
+			type: 'Program',
+			start: 0,
+			end: 8,
+			body: [
+				{
+					type: 'ExpressionStatement',
+					start: 0,
+					end: 5,
+					expression: {
+						type: 'Literal',
+						start: 0,
+						end: 4,
+						value: null,
+						raw: 'null'
+					}
+				},
+				{
+					type: 'ExpressionStatement',
+					start: 6,
+					end: 8,
+					expression: {
+						type: 'Literal',
+						start: 6,
+						end: 7,
+						value: 1,
+						raw: '1'
+					}
+				}
+			],
+			sourceType: 'module'
+		};
+
+		walk( ast, {
+			enter () {},
+			leave () {}
 		});
+
+		assert.ok( true );
 	});
 });
