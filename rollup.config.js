850b66f1f99f78632b91b9c1a2b151a7aa6f5340
Rich Harris <richard.a.harris@gmail.com>
2020-12-08 10:52:50 -0500

    fix export paths, fix package.json generation

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,25 @@
 import sucrase from 'rollup-plugin-sucrase';
-import pkg from './package.json';
 
 function emitModulePackageFile() {
-  return {
-    name: 'emit-module-package-file',
-    generateBundle() {
-      this.emitFile({
-        type: 'asset',
-        fileName: 'package.json',
-        source: `{"type":"module"}`
-      });
-    }
-  };
+	return {
+		name: 'emit-module-package-file',
+		generateBundle() {
+			this.emitFile({
+				type: 'asset',
+				fileName: 'package.json',
+				source: `{"type":"module"}`
+			});
+		}
+	};
 }
 
 export default {
-	input: 'src/index.ts',
+	input: {
+		'estree-walker': 'src/index.ts'
+	},
 	output: [
-		{ file: pkg.main, format: 'umd', name: 'estreeWalker' },
-		{ file: pkg.module, format: 'esm', plugins: [emitModulePackageFile] }
+		{ dir: 'dist/umd', format: 'umd', name: 'estreeWalker' },
+		{ dir: 'dist/esm', format: 'esm', plugins: [emitModulePackageFile()] }
 	],
 	plugins: [
 		sucrase({
